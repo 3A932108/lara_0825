d8c631f7abadd565bb7140eefff7856b0354b92f
yi chu hsu <b28203598@yahoo.com.tw>
2022-11-04 00:45:07 +0800

    新增post.blade.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/post', function () {
+    return view('post');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
