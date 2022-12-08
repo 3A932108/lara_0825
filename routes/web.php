c66d53ee75d542ce7926f829f737576aad1f8209
yi chu hsu <b28203598@yahoo.com.tw>
2022-12-08 23:10:28 +0800

    Model跟Collection的差異

--- routes/web.php
+++ routes/web.php
@@ -15,56 +15,21 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', function () {
-    /*新增db資料;
-    $post = new Post();
-    $post->title = "test_title";
-    $post->content = "test_content";
-    $post->save();
 
-    /*create方式儲存資料
-    Post::create([
-        'title' => 'created title',
-        'content' => 'created content',]);
-    return 'Saved success.';
-    /*3-1用find方法查詢post(只能找1筆)
-       $post = Post::find(1);
-       echo '標題' .$post->title. '<br>';
-       echo '內容' .$post->content. '<br>';
-       dd($post);*/
+    /*取得collection */
+   // $allPosts = Post::all();
+   // dd($allPosts);
 
+    //$featuredPosts = Post::where('is_feature', 1)->get();
+    //dd($featuredPosts);多筆
 
-    /*3-2用all方法查尋post(全部都可以找)
-    $posts = Post::all();
-    foreach ($posts as $post)
-    {
-    echo '編號' .$post->id. '<br>';
-    echo '標題' .$post->title. '<br>';
-    echo '內容' .$post->content. '<br>';
-    echo '張貼時間' .$post->create_at. '<br>';
-    echo '------------------------'. '<br>';
-    }
-    dd($posts);
-    */
+    //$fourthPost = Post::find(8);
+    //dd($fourthPost);單筆
 
-    /*3-3用where方法查尋id小於10的貼文，並遞減排序
-    $posts = Post::where('id','<','10')->orderBy('id','DESC')->get();
-    dd($posts);
-   /*save方式更新資料
-    $post = Post::find(1);
-    $post->title = 'saved title ';
-    $post->content = 'saved content';
-    $post->save();
-});*/
-    /*P5刪除DB資料
-     *用delete刪除一筆資料
-    $post = Post::find(1);
-    $post->delete();
-    return 'Delete success.';*/
+    $lastPost =Post::orderBy('id','DESC')->first();
+    dd($lastPost);
 
-    /*用destroy刪除單筆或多筆資料*/
-
-    Post::destroy(3,4,5,6,7,8,9,10);
-    return 'Delete success.';
+});
 
     Route::get('posts', [PostController::class, 'index'])
         ->name('posts.index');
@@ -74,4 +39,4 @@ Route::get('/', function () {
         ->name('posts.contact');
     Route::get('about', [PostController::class, 'about'])
         ->name('posts.about');
-});
+
